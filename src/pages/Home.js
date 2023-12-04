9153ab927c3dc381e21ddc24e3eb8416316a0fc9
Rei <rei@reiyua.lol>
2023-12-04 12:33:13 +0000

    Remove extrra references to "books" and replace with "art" & "artworks"

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,32 +2,29 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
-import { useState, useEffect, useContext } from "react"
+import { useState, useEffect } from "react"
 import { ItemImage } from "../components/ItemImage"
 
 export function Home(props) {
-    const [books,setBooks] = useState([])
+    const [artworks,setArtworks] = useState([])
 
     useEffect(() => {
       console.log( props.items )
-      setBooks( props.items )
+      setArtworks( props.items )
     }, [props.items])
   
   
   
-    const ItemCards = books.map( ( book, itemkey ) => {
-      const itemLink = `/detail/${book.id}`
+    const ItemCards = artworks.map( ( artwork, itemkey ) => {
+      const itemLink = `/detail/${artwork.id}`
       return(
         <Col md={4} className="mb-4" key={itemkey}>
           <Card key={itemkey} className="position-relative">
             <a href={itemLink} className="position-absolute" style={{top:0, left:0, right:0, bottom:0}}>
             </a>
-            <ItemImage source={ book.cover_image} />
+            <ItemImage source={ artwork.cover_image} />
             <Card.Body>
-              <Card.Title>{ book.book_title }</Card.Title>
-              <Card.Text>
-                By {book.author}
-              </Card.Text>
+              <Card.Title>{ artwork.artwork_title }</Card.Title>
             </Card.Body>
           </Card>
         </Col>
