4c51550ba73ea24a824de9fdd8796878ea3cfb07
reiyua <rei@reiyua.lol>
2024-01-23 07:20:38 +0000

    Add extra comments for future reference.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,6 +7,7 @@ export function Contact ( props ) {
         <div>
             <h1>Contact</h1>
             <p>Deciding whether to just put contact info or actually develop a contact form, figure it out along the way</p>
+            <p> If using contact form, either use EmailJS or use Google Firebase Triggers plus custom form to connect to said Firebase Triggers to send email. </p>
         </div>
     )
 }
